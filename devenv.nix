f971c269cce2b8d45e7bfd979d8d60c96eae22e9
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-08-26 11:03:33 +0200

    chore: migrate to devenv
    Signed-off-by: Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>

---
+++ devenv.nix
@@ -0,0 +1,41 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+let
+  pwd = config.env.DEVENV_ROOT;
+in
+{
+  name = "hive";
+
+  dotenv.enable = true;
+
+  env = {
+    "HELM_PLUGINS" = config.env.DEVENV_PROFILE;
+    "KUBECONFIG" = "${pwd}/kubeconfig";
+    "TALOSCONFIG" = "${pwd}/kubernetes/talos/clusterconfig/talosconfig";
+    "ROOT_DIR" = "${pwd}";
+  };
+
+  packages = [
+    pkgs.go-task
+    pkgs.helmfile
+    pkgs._1password-cli
+    pkgs.kubectl
+    pkgs.fluxcd
+    pkgs.kubernetes-helm
+    pkgs.kubernetes-helmPlugins.helm-diff
+    pkgs.cilium-cli
+    pkgs.minijinja
+    pkgs.talhelper
+    pkgs.kubefetch
+    pkgs.talosctl
+    pkgs.yq-go
+    pkgs.kubectl-node-shell
+
+    inputs.talhelper.packages.${pkgs.system}.default
+  ];
+}
